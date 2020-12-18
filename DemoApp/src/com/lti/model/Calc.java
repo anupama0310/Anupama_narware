8af53ebd0784dd34742ce0892ef319482afbb81b
anupama0310 <58244308+anupama0310@users.noreply.github.com>
2020-12-18 13:05:54 +0530

    Update Calc.java

--- DemoApp/src/com/lti/model/Calc.java
+++ DemoApp/src/com/lti/model/Calc.java
@@ -8,4 +8,7 @@ public class Calc {
 	public void sub(int num1, int num2){
 		System.out.println("Differnce="+(num1-num2));
 	}
+	public void multi(int num1, int num2){
+		System.out.println("Multiplication="+(num1*num2));
+	
 }
